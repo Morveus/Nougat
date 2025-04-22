5d657085fc980b38128d319e1092748e1c366d15
David Balland <morveus@morveus.com>
2025-04-22 17:20:55 +0200

    Fixed artifacts from previous removal

--- games/urls.py
+++ games/urls.py
@@ -6,7 +6,5 @@ urlpatterns = [
     path('create/', views.GameCreateView.as_view(), name='game-create'),
     path('<int:pk>/update/', views.GameUpdateView.as_view(), name='game-update'),
     path('<int:pk>/delete/', views.GameDeleteView.as_view(), name='game-delete'),
-    path('api-key/', views.APIKeyView.as_view(), name='api-key'),
-    path('ai-game-info/', views.AIGameInfoView.as_view(), name='ai-game-info'),
     path('<int:pk>/detail/', views.GameDetailView.as_view(), name='game-detail'),
 ] 
