904724b7fc713ec00a435625535695b81900e281
David Balland <morveus@morveus.com>
2025-04-22 17:47:47 +0200

    Fixed split in CSRF values

--- gamecollection/settings.py
+++ gamecollection/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 # Get CSRF trusted origins from environment variable
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
 
 
 # Application definition
